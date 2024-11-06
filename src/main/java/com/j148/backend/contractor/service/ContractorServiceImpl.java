4694bb2144134af6670c1d7ceb4034860284d516
Martinezpardon <pardonshikwambana@gmail.com>
2024-11-06 15:09:11 +0200

    changed ContractorRepository to ContractorRepo on ContractorServiceImpl imports to match changed class name

--- src/main/java/com/j148/backend/contractor/service/ContractorServiceImpl.java
+++ src/main/java/com/j148/backend/contractor/service/ContractorServiceImpl.java
@@ -2,19 +2,19 @@ package com.j148.backend.contractor.service;
 
 import com.j148.backend.contractor.model.Contractor;
 
-import com.j148.backend.contractor.repo.ContractorRepository;
+import com.j148.backend.contractor.repo.ContractorRepo;
 import java.sql.SQLException;
 import java.util.Optional;
 
 public class ContractorServiceImpl implements ContractorService{
-    private final ContractorRepository contractorRepository;
+    private final ContractorRepo contractorRepo;
 
-    public ContractorServiceImpl(ContractorRepository contractorRepository) {
-        this.contractorRepository = contractorRepository;
+    public ContractorServiceImpl(ContractorRepo contractorRepo) {
+        this.contractorRepo = contractorRepo;
     }
 
     @Override
     public Optional<Contractor> update(Contractor contractor) throws SQLException {
-        return contractorRepository.update(contractor);
+        return contractorRepo.update(contractor);
     }
 }
