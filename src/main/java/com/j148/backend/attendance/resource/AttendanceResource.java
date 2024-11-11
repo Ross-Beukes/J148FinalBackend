a87edb45677636c584079309ccac39e45177f7ef
Lalo223-spec <mulalomolea3@gmail.com>
2024-11-11 10:12:31 +0200

    Changes to AttendanceResource

--- src/main/java/com/j148/backend/attendance/resource/AttendanceResource.java
+++ src/main/java/com/j148/backend/attendance/resource/AttendanceResource.java
@@ -3,6 +3,7 @@ package com.j148.backend.attendance.resource;
 import com.j148.backend.attendance.model.Attendance;
 import com.j148.backend.attendance.service.AttendanceService;
 import com.j148.backend.attendance.service.AttendanceServiceImpl;
+import com.j148.backend.contractor.model.Contractor;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
 
@@ -14,145 +15,132 @@ import java.util.logging.Logger;
 
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
-@Path("/attendance")
-public class AttendanceResource {
-private static final Logger LOG = Logger.getLogger(AttendanceResource.class.getName());
+public class AttendanceResource{
 
-private AttendanceService attendanceService;
-/**
- * create a new attendance record
- *
- * @param attendance the attendance object to be inserted
- * @return Response with status of the operation
- * */
-@POST
-@Consumes(APPLICATION_JSON)
-@Produces(APPLICATION_JSON)
-public Response createAttendanceRecord (Attendance attendance){
-    try{
-        Optional<Attendance> createdAttendance = attendanceService.createAttendanceRecord(attendance);
-        if (createdAttendance.isPresent()){
+    private static  final Logger LOG = Logger.getLogger(AttendanceResource.class.getName());
+    private final AttendanceService attendanceService = new AttendanceServiceImpl();
+
+    /**
+     *Check in for a contractor - Creates attendance record.
+     *
+     * @param attendance The attendance record details.
+     * @return Response with status of the operation
+     **/
+    @POST
+    @Path("/checkin")
+    @Consumes(APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
+    public Response checkIn(Attendance attendance){
+        try{
+            Attendance createdAttendance = attendanceService.createAttendenceRecord(attendance);
             return Response.status(Response.Status.CREATED)
-                    .entity(createdAttendance.get())
+                    .entity(createdAttendance)
                     .build();
-
-        }else {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("Failed to create attendance record")
+        } catch (SQLException e){
+            LOG.log(Level.SEVERE,"Error while creating attendance record",e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Error while processing the check-in request.")
+                    .build();
+        } catch (Exception e){
+            LOG.log(Level.SEVERE,"Error while creating attendance record",e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Error while processing the check-in request.")
                     .build();
         }
-    }catch (SQLException e){
-        LOG.log(Level.SEVERE,"Error while create attendance record ",e);
-        return  Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                .entity("Error while processing the request.")
-                .build();
     }
-}
-/**
- * Retrieve an attendance record by its ID
- *
- * @param id the attendance with attendance record or error message.
- * */
-@GET
-@Path("/{id}")
-@Produces(APPLICATION_JSON)
-public Response getAttendanceById(@PathParam("id")Long id){
-    try {
-        Optional<Attendance>attendance =attendanceService.getAttendanceById(id);
-    if (attendance.isPresent()){
-        return Response.status(Response.Status.OK)
-                .entity(attendance.get())
-                .build();
-    }else {
-        return Response.status(Response.Status.NOT_FOUND)
-                .entity("Attendance Record not found ")
-                .build();
-    }
-
-    }catch (SQLException e){
-        LOG.log(Level.SEVERE,"Error while retrieving attendance record with ID"+id+e);
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                .entity("Error while processing the request")
-                .build();
-    }
-}
-/**
- * Update an existing attendance record
- *
- * @param attendance the attendance object with the updated details
- * @return Response with the status of the update operation
- *
- * */
-@PUT
-@Consumes(APPLICATION_JSON)
-@Produces(APPLICATION_JSON)
-public Response updateAttendance(Attendance attendance){
+    /**
+     * Check out of for a contractor - Updates the time-out in an attendance record.
+     *
+     * @param attendance The attendance record to be checked out
+     * @return Response with the status of the update operation*/
+    @POST
+    @Path("/checkout")
+    @Consumes(APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
+    public Response checkOut(Attendance attendance){
     try{
-        Optional<Attendance> updateAttendance =attendanceService.updateAttendance(attendance);
-        if (updateAttendance.isPresent()){
+        Attendance updatedAttendance = attendanceService.checkOut(attendance);
+        return   Response.status(Response.Status.OK)
+                .entity(updatedAttendance)
+                .build();
+    } catch(SQLException e){
+        LOG.log(Level.SEVERE,"Error while checking out attendance record",e);
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity("Error while processing the check-out request")
+                .build();
+
+    } catch(Exception e){
+        LOG.log(Level.SEVERE,"Error while checking out attendance record",e);
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity("Error while processing the check-out request")
+                .build();
+
+    }
+    }
+    /**
+     * Mark absent contractors for today
+     *
+     * @return Response with a list of absent attendance records*/
+    @POST
+    @Path("/absent")
+    @Produces(APPLICATION_JSON)
+        public Response markAbsentContractors(){
+        try{
+            List<Attendance>absentContractors =attendanceService.createAbsentContractors();
             return Response.status(Response.Status.OK)
-                    .entity(updateAttendance.get())
+                    .entity(absentContractors)
                     .build();
 
-        }else {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("Failed to update Record ")
+        }catch (SQLException e){
+            LOG.log(Level.SEVERE, "Error while absent contractors ",e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Error while processing absent contractors")
                     .build();
-        }
-    }catch (SQLException e){
-LOG.log(Level.SEVERE,"Error while updating attendance record ",e);
-return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-        .entity("Error while processing request")
-        .build();
-    }
-}
 
-/**
- * Retrieve all attendance records
- *
- * @reurn Response with a list of all attendance records */
-public Response getAllAttendanceRecords(){
-    try{
-        List<Attendance>attendanceList= attendanceService.getAllAttendanceRecords();
-        if (!attendanceList.isEmpty()){
-            return Response.status(Response.Status.OK)
-                    .entity(attendanceList)
-                    .build();
-        }else{
-            return Response.status(Response.Status.NOT_FOUND)
-                    .entity("No attendance record found ")
+        }catch (Exception e ){
+            LOG.log(Level.SEVERE, "Error while absent contractors ",e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Error while processing absent contractors")
                     .build();
         }
-    }catch (SQLException e){
-        LOG.log(Level.SEVERE,"Error while retrieving all attendance records",e);
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                .entity("Error while processing the request.")
-                .build();
     }
-}
-/**
- * Retrieve all attendance records for a specific contractor
- *
- * @param contractor_id the contractors ID
- * @return Response with the attendance records of the contractor */
-public Response getAllAttendanceForContractor(@PathParam("contractor_id")Long contractor_id){
-    try{
-        Optional<Attendance> contractorAttendance = attendanceService.FindAllAttendanceForContractor(contractor_id);
-        if(contractorAttendance.isPresent()){
+    /**
+     * Get list of contractors who haven't checked in today.
+     *
+     * @param contractors List of contractors to check
+     * @return Response with list of contractors who haven't checked in today*/
+
+    @POST
+    @Path("/notcheckedin")
+    @Consumes(APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
+    public Response getContractorsNotCheckedIn(List<Contractor>contractors){
+        try{
+            List<Attendance>notCheckedIn= attendanceService.contractorsNotCheckedIn(contractors);
             return Response.status(Response.Status.OK)
-                    .entity(contractorAttendance.get())
+                    .entity(notCheckedIn)
                     .build();
-        }else {
-            return Response.status(Response.Status.NOT_FOUND)
-                    .entity("No attendance records found gor this contractor")
+
+        }catch (SQLException e){
+            LOG.log(Level.SEVERE,"Error while retrieving contractors not checked in");
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Error while processing contractor not checked in")
                     .build();
+
+        }catch(Exception e){
+            LOG.log(Level.SEVERE,"Error while retrieving contractors not checked in");
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Error while processing contractor not checked in")
+                    .build();
+
         }
-    }catch (SQLException e){
-        LOG.log(Level.SEVERE,"Error while retrieving attendance for this contractor ",e);
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                .entity("Error while processing the request.")
-                .build();
     }
-}
+
+
+
+
+
+
+
 
 }
