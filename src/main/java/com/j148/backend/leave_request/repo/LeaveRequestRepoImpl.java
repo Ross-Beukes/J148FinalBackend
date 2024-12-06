ebd7538c5a4623e1d0b5a539834ae87f9957a603
HangwelaniMadilonga <hangwelani3@gmail.com>
2024-12-06 10:22:57 +0200

    Update.

--- src/main/java/com/j148/backend/leave_request/repo/LeaveRequestRepoImpl.java
+++ src/main/java/com/j148/backend/leave_request/repo/LeaveRequestRepoImpl.java
@@ -139,7 +139,7 @@ public class LeaveRequestRepoImpl   implements LeaveRequestRepo {
                 "FROM leave_request lr " +
                 "JOIN contractor c ON lr.contractor_id = c.contractor_id " +
                 "JOIN user u ON c.user_id = u.user_id " +
-                "WHERE lr.decision = PENDING";
+                "WHERE lr.decision = \"PENDING\"";
         try (Connection con = DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query); ResultSet rs = ps.executeQuery()) {
             while (rs.next()) {
                 LeaveRequest leaveRequest = LeaveRequest.builder()
