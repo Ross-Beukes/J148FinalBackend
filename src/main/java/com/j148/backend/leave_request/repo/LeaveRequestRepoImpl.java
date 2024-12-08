bb48f2b5a985083660ec831ace3749c0d6b9cba8
Naledi Modika <naledi.modika@icloud.com>
2024-12-08 14:37:47 +0200

    fix errors in leave request

--- src/main/java/com/j148/backend/leave_request/repo/LeaveRequestRepoImpl.java
+++ src/main/java/com/j148/backend/leave_request/repo/LeaveRequestRepoImpl.java
@@ -9,13 +9,13 @@ import com.j148.backend.contractor.model.Contractor;
 import com.j148.backend.files.model.FileEntity;
 import com.j148.backend.leave_request.model.LeaveRequest;
 import com.j148.backend.user.model.User;
-import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Savepoint;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.AbstractMap;
@@ -27,23 +27,23 @@ import java.util.logging.Logger;
 /**
  * @author yusuf
  */
-@ApplicationScoped
-public class LeaveRequestRepoImpl   implements LeaveRequestRepo {
+public class LeaveRequestRepoImpl implements LeaveRequestRepo {
 
     @Inject
     private DBConfig DBConfig;
 
-
-
     @Override
     public Optional<LeaveRequest> createLeaveRequest(LeaveRequest leaveRequest) throws SQLException {
         String query = "INSERT INTO leave_request (contractor_id, start_date, end_date, decision) VALUES(?, ?, ?, ?)";
-        try (Connection con =  DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+        try (Connection con = DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
             ps.setLong(1, leaveRequest.getContractor().getContractorId());
             ps.setString(2, String.valueOf(leaveRequest.getStartDate()));
             ps.setString(3, String.valueOf(leaveRequest.getEndDate()));
             ps.setString(4, "PENDING");
+
+            Savepoint beforeReservationInput = con.setSavepoint();
             if (ps.executeUpdate() > 0) {
+                con.commit();
                 try (ResultSet keys = ps.getGeneratedKeys()) {
                     if (keys.next()) {
                         leaveRequest.setLeaveRequestId(keys.getLong(1));
@@ -51,6 +51,8 @@ public class LeaveRequestRepoImpl   implements LeaveRequestRepo {
                 }
 
                 return Optional.of(leaveRequest);
+            } else {
+                con.rollback(beforeReservationInput);
             }
         }
         return Optional.empty();
@@ -60,7 +62,7 @@ public class LeaveRequestRepoImpl   implements LeaveRequestRepo {
     public AbstractMap<Long, LeaveRequest> retrieveAllContractorLeaveRequests(Contractor contractor) throws SQLException {
         HashMap<Long, LeaveRequest> requestMap = new HashMap<>();
         String query = "SELECT * FROM leave_request WHERE contractor_id = ?";
-        try (Connection con =  DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query)) {
+        try (Connection con = DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query)) {
             ps.setLong(1, contractor.getContractorId());
             try (ResultSet rs = ps.executeQuery()) {
                 while (rs.next()) {
@@ -79,7 +81,7 @@ public class LeaveRequestRepoImpl   implements LeaveRequestRepo {
     public AbstractMap<Long, LeaveRequest> retrieveLeaveRequestsByStartAndEndDate(LocalDate startDate, LocalDate endDate) throws SQLException {
         HashMap<Long, LeaveRequest> requestMap = new HashMap<>();
         String query = "SELECT * FROM leave_request WHERE start_date > ? AND end_date < ?";
-        try (Connection con =  DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query)) {
+        try (Connection con = DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query)) {
             ps.setString(1, String.valueOf(startDate));
             ps.setString(2, String.valueOf(endDate));
             try (ResultSet rs = ps.executeQuery()) {
@@ -100,15 +102,21 @@ public class LeaveRequestRepoImpl   implements LeaveRequestRepo {
     public Optional<LeaveRequest> updateLeaveRequestToApprovedOrDenied(LeaveRequest leaveRequest) throws SQLException {
         String query = "UPDATE leave_request SET decision = ? WHERE leave_request_id = ?";
 
-        try (Connection con =  DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query)) {
+        try (Connection con = DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query)) {
+
+            Savepoint beforeReservationInput = con.setSavepoint();
 
             ps.setString(1, leaveRequest.getDecision().toString());
             ps.setLong(2, leaveRequest.getLeaveRequestId());
 
-            if (ps.executeUpdate() > 0) {
+            con.setAutoCommit(false);
 
+            if (ps.executeUpdate() > 0) {
+                con.commit();
 
                 return Optional.of(leaveRequest);
+            } else {
+                con.rollback(beforeReservationInput);
             }
         }
         return Optional.empty();
@@ -118,12 +126,12 @@ public class LeaveRequestRepoImpl   implements LeaveRequestRepo {
     public AbstractMap<Long, LeaveRequest> retrieveAll() throws SQLException {
         HashMap<Long, LeaveRequest> requestMap = new HashMap<>();
         String query = "SELECT * FROM leave_request";
-        try (Connection con =  DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query); ResultSet rs = ps.executeQuery()) {
+        try (Connection con = DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query); ResultSet rs = ps.executeQuery()) {
             while (rs.next()) {
-//                requestMap.keySet().add(rs.getLong("leave_request_id"));
                 requestMap.put(rs.getLong("leave_request_id"), LeaveRequest.builder().startDate(rs.getDate("start_date").toLocalDate())
                         .endDate(rs.getDate("end_date").toLocalDate()).decision(LeaveRequest.Decision.valueOf(rs.getString("decision")))
-                        .contractor(Contractor.builder().contractorId(rs.getLong("contractor_id")).build()).build());
+                        .contractor(Contractor.builder().contractorId(rs.getLong("contractor_id")).build())
+                        .build());
 
             }
         }
@@ -134,12 +142,12 @@ public class LeaveRequestRepoImpl   implements LeaveRequestRepo {
     public ArrayList<LeaveRequest> retrieveAllLeaveRequest() throws SQLException {
         ArrayList<LeaveRequest> leaveRequests = new ArrayList<>();
         Logger logger = Logger.getLogger(this.getClass().getName());
-        String query = "SELECT lr.leave_request_id, lr.start_date, lr.end_date, lr.decision, " +
-                "u.name, u.email " +
-                "FROM leave_request lr " +
-                "JOIN contractor c ON lr.contractor_id = c.contractor_id " +
-                "JOIN user u ON c.user_id = u.user_id " +
-                "WHERE lr.decision = \"PENDING\"";
+        String query = "SELECT lr.leave_request_id, lr.start_date, lr.end_date, lr.decision, "
+                + "u.name, u.email "
+                + "FROM leave_request lr "
+                + "JOIN contractor c ON lr.contractor_id = c.contractor_id "
+                + "JOIN user u ON c.user_id = u.user_id "
+                + "WHERE lr.decision = \"PENDING\"";
         try (Connection con = DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query); ResultSet rs = ps.executeQuery()) {
             while (rs.next()) {
                 LeaveRequest leaveRequest = LeaveRequest.builder()
@@ -155,7 +163,6 @@ public class LeaveRequestRepoImpl   implements LeaveRequestRepo {
                                 .build())
                         .build();
                 leaveRequests.add(leaveRequest);
-                System.out.println(leaveRequest);
             }
             logger.info("Total leave requests retrieved: " + leaveRequests.size());
         }
@@ -167,7 +174,7 @@ public class LeaveRequestRepoImpl   implements LeaveRequestRepo {
     public AbstractMap<Long, LeaveRequest> retrieveAllPendingContractorLeaveRequests(Contractor contractor) throws SQLException {
         HashMap<Long, LeaveRequest> requestMap = new HashMap<>();
         String query = "SELECT * FROM leave_request WHERE contractor_id = ? AND decision = \"PENDING\"";
-        try (Connection con =  DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query)) {
+        try (Connection con = DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query)) {
             ps.setLong(1, contractor.getContractorId());
             try (ResultSet rs = ps.executeQuery()) {
                 while (rs.next()) {
@@ -187,7 +194,7 @@ public class LeaveRequestRepoImpl   implements LeaveRequestRepo {
     public AbstractMap<Long, LeaveRequest> retrieveAllLeaveRequestsByDecision(String decision) throws SQLException {
         HashMap<Long, LeaveRequest> requestMap = new HashMap<>();
         String query = "SELECT * FROM leave_request WHERE decision = ?";
-        try (Connection con =  DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query)) {
+        try (Connection con = DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query)) {
             ps.setString(1, LeaveRequest.Decision.valueOf(decision).toString());
             try (ResultSet rs = ps.executeQuery()) {
                 while (rs.next()) {
@@ -206,7 +213,7 @@ public class LeaveRequestRepoImpl   implements LeaveRequestRepo {
     @Override
     public Optional<LeaveRequest> retrieveLeaveRequestByID(LeaveRequest leaveRequest) throws SQLException {
         String query = "SELECT * FROM leave_request WHERE leave_request_id = ?";
-        try (Connection con =  DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query)) {
+        try (Connection con = DBConfig.getCon(); PreparedStatement ps = con.prepareStatement(query)) {
             ps.setLong(1, leaveRequest.getLeaveRequestId());
             try (ResultSet rs = ps.executeQuery()) {
                 if (rs.next()) {
@@ -221,6 +228,5 @@ public class LeaveRequestRepoImpl   implements LeaveRequestRepo {
         }
         return Optional.empty();
     }
-
 
 }
