7f2a2b3a38b50fb9086aadd16c1e2e0a900da151
HangwelaniMadilonga <hangwelani3@gmail.com>
2024-12-09 11:29:27 +0200

    Fix sql error on LeaveRequestRepoImpl.

--- src/main/java/com/j148/backend/leave_request/repo/LeaveRequestRepoImpl.java
+++ src/main/java/com/j148/backend/leave_request/repo/LeaveRequestRepoImpl.java
@@ -135,7 +135,8 @@ public class LeaveRequestRepoImpl   implements LeaveRequestRepo {
         ArrayList<LeaveRequest> leaveRequests = new ArrayList<>();
         Logger logger = Logger.getLogger(this.getClass().getName());
         String query = "SELECT lr.leave_request_id, lr.start_date, lr.end_date, lr.decision, " +
-                "u.name, u.email " +
+                "u.name, u.surname, u.email, " +
+                "c.contractor_id " +
                 "FROM leave_request lr " +
                 "JOIN contractor c ON lr.contractor_id = c.contractor_id " +
                 "JOIN user u ON c.user_id = u.user_id " +
@@ -151,6 +152,7 @@ public class LeaveRequestRepoImpl   implements LeaveRequestRepo {
                                 .contractorId(rs.getLong("contractor_id"))
                                 .user(User.builder()
                                         .name(rs.getString("name"))
+                                        .surname(rs.getString("surname"))
                                         .email(rs.getString("email"))
                                         .build())
                                 .build())
